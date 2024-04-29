b19afba1de8c9f49f8007193429f88f8e71d18ff
Boris Berkgaut <berkgaut@gmail.com>
2024-04-29 23:35:00 +0200

    examples/unix-shell

--- examples/unix-shell/mnb-spec.py
+++ examples/unix-shell/mnb-spec.py
@@ -3,7 +3,7 @@ from spec import *
 from spec_json import print_spec_json
 
 s = Spec(spec_version=(1,0))
-file_1 = "examples/unix-shell/mnb-generated/file.txt"
+file_1 = "mnb-generated/file.txt"
 bash_image = "bash:5.2"
 s.pull_image(bash_image)
 
@@ -12,7 +12,7 @@ cmd1.output(file_1, through_stdout=True)
 
 cmd2 = s.exec(bash_image, command=["bash", "-c", '>&2 echo "X=${X}"; echo "Hello stdout!"'])
 cmd2.input(file=file_1, through_env='X')
-cmd2.output(file="examples/unix-shell/mnb-generated/stderr.txt", through_stderr=True)
-cmd2.output(file="examples/unix-shell/mnb-generated/stdout.txt", through_stdout=True)
+cmd2.output(file="mnb-generated/stderr.txt", through_stderr=True)
+cmd2.output(file="mnb-generated/stdout.txt", through_stdout=True)
 
 print_spec_json(s)
